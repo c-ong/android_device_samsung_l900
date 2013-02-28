d2c09307af39c087b411271323a140de7ab93835
sbrissen <sbrissen@hotmail.com>
2013-02-28 15:58:05 -0500

    l900: add sprint service menu WIP
    Change-Id: I15f2c93c783cb86a914ff3118bf53704377b7b1d

--- app/Android.mk
+++ app/Android.mk
@@ -1,32 +1,30 @@
-#
+# Copyright (C) 2012 The Android Open Source Project
 # Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-LOCAL_PATH := device/samsung/l900
+LOCAL_PATH := $(call my-dir)
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+ifneq ($(filter l900,$(TARGET_DEVICE)),)
 
-# Init files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.smdk4x12:root/fstab.smdk4x12
+include $(CLEAR_VARS)
+LOCAL_MODULE := SDM
+LOCAL_MODULE_OWNER := samsung
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := platform
+include $(BUILD_PREBUILT)
 
-PRODUCT_PACKAGES += \
-    SDM
-
-# Include common makefile
-$(call inherit-product, device/samsung/t0ltecdma/t0ltecdma.mk)
-
-$(call inherit-product-if-exists, vendor/samsung/l900/l900-vendor.mk)
+endif
